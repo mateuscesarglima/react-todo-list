e7eea76d55cc2e6da1cda9d300fbf61ded7b1666
Mateus Cesar <mateuscesarglima@gmail.com>
2023-10-27 19:22:54 -0300

    feat(TCC-2): update todo list mfe

---
+++ src/container/app.tsx
@@ -0,0 +1,11 @@
+//@ts-ignore
+import { ListContainer } from '@mfe/todo-styleguide';
+
+
+const App = ({ props }) => {
+  return (
+    <ListContainer />
+  );
+}
+
+export default App;
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,5 @@
+import App from "./container/app";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return <App props={props} />
 }
